8b5699b17f54a8e6c1b02c2985550e7ccdae1cc8
Emu-L8r <marcuswb3@gmail.com>
2026-03-19 16:31:15 +0800

    Update CustomerListViewModelTest to use consolidated CustomerListViewModel and CustomerListUiState

---
+++ path/to/CustomerListViewModelTest.kt
@@ -0,0 +1,6 @@
+// Updated test to use consolidated CustomerListViewModel and CustomerListUiState
+import ... // your imports here
+
+class CustomerListViewModelTest {
+    // Your test code here
+}
