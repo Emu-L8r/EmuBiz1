bc033b378b42f8e253c928d7ca95de6441d39c1a
Emu-L8r <marcuswb3@gmail.com>
2026-03-20 20:17:01 +0800

    Add missing navigation helper functions

---
+++ NavExtensionsV2.kt
@@ -0,0 +1,21 @@
+// Navigation Helper Functions
+
+fun navigateToCustomerDetailV2(customerId: String) {
+    // Implementation here
+}
+
+fun navigateToInvoiceDetailV2(invoiceId: String) {
+    // Implementation here
+}
+
+fun navigateToRiskAnalyticsV2() {
+    // Implementation here
+}
+
+fun navigateToBackupRestoreV2() {
+    // Implementation here
+}
+
+fun navigateToVaultV2() {
+    // Implementation here
+}
